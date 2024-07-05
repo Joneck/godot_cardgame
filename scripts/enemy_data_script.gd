d3a1e5b5c9f38eb65557dd9a954e259516d0b587
Eryk Dziemidzionek <eryk.dziemidzionek@gmail.com>
2024-07-05 17:20:07 +0200

    Initial commit

---
+++ scripts/enemy_data_script.gd
@@ -0,0 +1,12 @@
+extends Sprite2D
+
+var type = 'Goblin'
+var lifePoints = 10
+
+func death():
+	pass
+	
+func _draw():
+	#wstawia odpowiedniego sprita zależnie od type
+	pass
+	
---
+++ scripts/player_script.gd
@@ -0,0 +1,13 @@
+
+extends Node2D
+
+var lifePoints = 50
+var mana = 3
+
+
+func _process(delta):
+	rotation += PI * delta * 20
+	pass
+
+func death():
+	pass

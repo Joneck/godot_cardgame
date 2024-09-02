8bb504da424a4d4e39589d8d05dcaf9e8c88aefc
Eryk Dziemidzionek <eryk.dziemidzionek@gmail.com>
2024-09-02 21:35:18 +0200

    Struggle to understand Control nodes

--- scenes/map/map_body/map_body_vbox.gd
+++ scenes/map/map_body/map_body_vbox.gd
@@ -3,7 +3,7 @@ extends VBoxContainer
 var rooms = [preload('res://scenes/map/map_questionMark/map_questionMark.tscn')]
 
 func generate():
-	for i in range(5):
+	for i in range(10):
 		var instance = rooms[0].instantiate()
 		add_child(instance)
 		instance.custom_minimum_size = Vector2(100,100)
